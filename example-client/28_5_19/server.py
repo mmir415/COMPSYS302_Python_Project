0debbbfd513fc0bdbde5dfeb3e893b1ff7084630
mmir415 <41189871+mmir415@users.noreply.github.com>
2019-05-31 20:55:42 +1200

    Modified to kind of work for ping, report, and broadcast

--- example-client/28_5_19/server.py
+++ example-client/28_5_19/server.py
@@ -67,6 +67,9 @@ class MainApp(object):
         if error == 0:
             cherrypy.session['username'] = username
             cherrypy.session['password'] = password
+            report(username,password)
+            print("Watch Carole & Tuesday")
+            broadcast(username,password)
         #    pubkeyAutho()
             raise cherrypy.HTTPRedirect('/')
         else:
@@ -184,8 +187,11 @@ def ping(username,password):
         response.close()
 
         JSON_object = json.loads(data.decode(encoding))
-        print(JSON_object)
-
+        if (JSON_object["authentication"] == "error"):
+            return 1
+        else:
+            print(json.dumps(JSON_object,indent=4))
+            return 0
     
 def get_privatedata(username,password):
 
@@ -214,7 +220,64 @@ def get_privatedata(username,password):
         response.close()
 
         JSON_object = json.loads(data.decode(encoding))
-        print(json.dumps(JSON_object,indent=4))
+        if JSON_object["privatedata"] == "7e74f2b1978473d9943b0178f3bfe538b215f84c99bc70ccf3ca67b0e3bc13a5":
+            print(json.dumps(JSON_object,indent=4))
+            return "7e74f2b1978473d9943b0178f3bfe538b215f84c99bc70ccf3ca67b0e3bc13a5"
+
+        else:
+            return 1
+
+
+def broadcast(username,password):
+    timing = str(time.time())
+    ENCODING = 'utf-8'
+
+    login_server_record = 'mmir415,7e74f2b1978473d9943b0178f3bfe538b215f84c99bc70ccf3ca67b0e3bc13a5,1558398219.422035,5326677c6a44df9bc95b2d62907b8bcc86b02f6c90dbbaeb4065089d66aec655f0b6e9eda3469ac09418160363cadda75c5a75577ead997b79ac6c3392722c0c'
+    signing_key = nacl.signing.SigningKey(key, encoder=nacl.encoding.HexEncoder)
+
+    # Serialize the verify key to send it to a third party
+#verify_key_hex = signing_key.encode(encoder=nacl.encoding.HexEncoder)
+    pubkey_hex = signing_key.verify_key.encode(encoder = nacl.encoding.HexEncoder)
+    
+    pubkey_hex_str = pubkey_hex.decode(ENCODING)
+
+    message = "Signed in from login_server"
+
+    message_bytes = bytes(login_server_record + message + timing, encoding=ENCODING)
+    signed = signing_key.sign(message_bytes, encoder=nacl.encoding.HexEncoder)
+
+    signature_hex_str = signed.signature.decode(ENCODING)
+
+    addkey_url = "http://cs302.kiwi.land/api/rx_broadcast"
+
+    credentials = ('%s:%s' % (username, password))
+    b64_credentials = base64.b64encode(credentials.encode('ascii'))
+    headers = {
+        'Authorization': 'Basic %s' % b64_credentials.decode('ascii'),
+        'Content-Type' : 'application/json; charset=utf-8',
+    }
+
+    payload = {
+        "loginserver_record": login_server_record,
+        "message": message,
+        "sender_created_at": timing,
+        "signature": signature_hex_str
+    }
+    json_payload = json.dumps(payload)
+    byte_payload = bytes(json_payload, ENCODING)
+
+    try:   
+        req = urllib.request.Request(url=addkey_url, data=byte_payload, headers=headers)
+        response = urllib.request.urlopen(req)
+    except urllib.error.HTTPError as err:
+        print("Error: " + str(err.code))
+    else:
+        data = response.read() # read the received bytes
+        encoding = response.info().get_content_charset(ENCODING) #load encoding if possible (default to utf-8)
+        response.close()
+
+        JSON_object = json.loads(data.decode(encoding))
+        print(JSON_object)
 
 def report(username,password):
 
@@ -267,13 +330,15 @@ def report(username,password):
 
 def authoriseUserLogin(username, password):
     print("Log on attempt from {0}:{1}".format(username, password))
-    ping(username,password)
-    get_privatedata(username,password)
-    report(username,password)
-    if ((username.lower() == "user") and (password.lower() == "password") or (username.lower() == "mmir415") and (password.lower() == "mmir415_339816700") ):
+    #ping(username,password)
+    pubhexstr = (get_privatedata(username,password))
+   # report(username,password)
+    return (ping(username,password))
+    
+   # if ((username.lower() == "user") and (password.lower() == "password") or (username.lower() == "mmir415") and (password.lower() == "mmir415_339816700") ):
   
-        print("Success")
-        return 0
-    else:
-        print("Failure")
-        return 1
+     #   print("Success")
+    #return 0
+   # else:
+   #     print("Failure")
+   #     return 1
