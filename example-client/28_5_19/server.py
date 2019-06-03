1472985579150432c65121b122fcd05a0c455542
mmir415 <41189871+mmir415@users.noreply.github.com>
2019-06-03 14:02:38 +1200

    Can successfully send message to Tomas and theoretically receive an encrypted message

--- example-client/28_5_19/server.py
+++ example-client/28_5_19/server.py
@@ -48,6 +48,36 @@ class apiList(object):
 
         return (response)
 
+    @cherrypy.expose              
+    def rx_privatemessage(self):
+        received_data = json.loads(cherrypy.request.body.read().decode('utf-8'))
+        print("Private Sender:")
+        sender_logins = (received_data)["loginserver_record"]
+        print (sender_logins)
+        login_list = (sender_logins.split(","))
+        print(login_list)
+        sender_name = login_list[0]
+        print(sender_name)
+
+        message = received_data.get('encrypted_message').encode('utf-8')
+        print("Private Message:")
+        print(message)
+        
+
+        response = {
+            'response':'ok'
+        }
+        response = json.dumps(response)
+        #for x in  (response)["loginserver_record"]:
+        
+          #  try:
+          #      ip_address = x.get("connection_address")
+          #      print(ip_address)
+
+        return (response)
+
+    
+
 
    
 
@@ -117,6 +147,11 @@ class MainApp(object):
                 except:
                     pass
             #MainApp.listusers(self,username,password)
+            try:
+                MainApp.private_message(self,username,password)
+            except:
+                print("Offline")
+                pass
             
                 
             MainApp.receive_message(self)
@@ -354,6 +389,77 @@ class MainApp(object):
 
             # response = json.dumps(response)
             # print(response)
+    def private_message(self,username,password):
+        server_pubkey = '67e5107702196a80bff43b46c25531bc7f0cbbb44db5d24bd89077387abc73b6'
+
+    #target_user = "admin"
+        target_user = "tant836"
+        target_ip = "172.24.5.136:1234"
+
+        login_server_record = 'mmir415,7e74f2b1978473d9943b0178f3bfe538b215f84c99bc70ccf3ca67b0e3bc13a5,1558398219.422035,5326677c6a44df9bc95b2d62907b8bcc86b02f6c90dbbaeb4065089d66aec655f0b6e9eda3469ac09418160363cadda75c5a75577ead997b79ac6c3392722c0c'
+        timing = str(time.time())
+        ENCODING = 'utf-8'
+
+
+        signing_key = nacl.signing.SigningKey(key, encoder=nacl.encoding.HexEncoder)
+
+            # Serialize the verify key to send it to a third party
+        #verify_key_hex = signing_key.encode(encoder=nacl.encoding.HexEncoder)
+        pubkey_hex = signing_key.verify_key.encode(encoder = nacl.encoding.HexEncoder)
+            
+        pubkey_hex_str = pubkey_hex.decode(ENCODING)
+
+        message = bytes("What pie would you like?",ENCODING)
+        #message = bytes((chr(128184)),ENCODING)
+
+        server_pubkey_bytes = bytes(server_pubkey,ENCODING)
+
+        vkey = nacl.signing.VerifyKey(server_pubkey_bytes, encoder=nacl.encoding.HexEncoder)
+        pub_key = vkey.to_curve25519_public_key()
+        sealed_box = nacl.public.SealedBox(pub_key)
+        encrypted = sealed_box.encrypt(message, encoder=nacl.encoding.HexEncoder)
+        en_message = encrypted.decode('utf-8')
+
+        message_bytes = bytes(login_server_record + server_pubkey + target_user + en_message + timing, encoding=ENCODING)
+        signed = signing_key.sign(message_bytes, encoder=nacl.encoding.HexEncoder)
+
+        signature_hex_str = signed.signature.decode(ENCODING)
+
+        addkey_url = "http://"+target_ip+"/api/rx_privatemessage"
+
+        credentials = ('%s:%s' % (username, password))
+        b64_credentials = base64.b64encode(credentials.encode('ascii'))
+        headers = {
+            'Authorization': 'Basic %s' % b64_credentials.decode('ascii'),
+            'Content-Type' : 'application/json; charset=utf-8',
+        }
+
+        payload = {
+            "loginserver_record": login_server_record,
+            "target_pubkey": server_pubkey,
+            "target_username": target_user,
+            "encrypted_message": en_message,
+            "sender_created_at": timing,
+            "signature": signature_hex_str
+        }
+        json_payload = json.dumps(payload)
+        byte_payload = bytes(json_payload, ENCODING)
+
+        try:   
+            req = urllib.request.Request(url=addkey_url, data=byte_payload, headers=headers)
+            response = urllib.request.urlopen(req)
+        except urllib.error.HTTPError as err:
+            print("Error: " + str(err.code))
+        else:
+            data = response.read() # read the received bytes
+            encoding = response.info().get_content_charset(ENCODING) #load encoding if possible (default to utf-8)
+            response.close()
+
+            JSON_object = json.loads(data.decode(encoding))
+            print(JSON_object)
+
+
+
 
     @cherrypy.expose
     def receive_message(self):
